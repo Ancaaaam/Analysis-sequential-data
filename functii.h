e41009ca212511895739dcfbbf8ec557d6c6e259
Ancaaaam <94039813+Ancaaaam@users.noreply.github.com>
2022-11-07 01:42:49 +0200

    Update functii.h

---
+++ functii.h
@@ -0,0 +1,16 @@
+#include "Tdouble_linked_list.h"
+
+#ifndef FUNCTII_H
+#define FUNCTII_H
+
+double average(node* p);
+
+double deviation(node* p, double ave);
+
+double sortare(TDoubleLinkedList** lista);
+
+double m_arit(TDoubleLinkedList** lista);
+
+void sortare_st(TDoubleLinkedList** lista);
+
+#endif
